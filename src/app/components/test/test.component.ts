5b2d33fb86154635ffccbb4823046d1653f91d42
Hector Camacho <danielcatu@gmail.com>
2020-06-10 10:48:50 -0500

    Vista de crear preguntas completada

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -6,11 +6,20 @@ import { TestService } from '../../services/test.service'
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
-
+  preguntas: Pregunta[];
   constructor(private test: TestService) { }
 
   ngOnInit(): void {
-    this.test.getQuestion();
+    this.getQuest();
+  }
+  async getQuest(){
+    this.preguntas=await this.test.getQuestion() as unknown as Pregunta[];
   }
 
+}
+class Pregunta {
+  question: string = "";
+  panswer: string[] = ["", "", "", ""];
+  answer: string = "";
+  value: string = "";
 }
