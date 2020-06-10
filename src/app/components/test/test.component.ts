635004fe97d8a8a4550a22a39c7625f783751b7c
Hector Camacho <danielcatu@gmail.com>
2020-06-10 12:25:28 -0500

    Preguntas empleados

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -7,13 +7,32 @@ import { TestService } from '../../services/test.service'
 })
 export class TestComponent implements OnInit {
   preguntas: Pregunta[];
-  constructor(private test: TestService) { }
+  answer: Answer;
+  constructor(private testService: TestService) {
+    this.answer = new Answer();
+  }
 
   ngOnInit(): void {
     this.getQuest();
   }
-  async getQuest(){
-    this.preguntas=await this.test.getQuestion() as unknown as Pregunta[];
+  async getQuest() {
+    this.preguntas = await this.testService.getQuestion() as unknown as Pregunta[];
+  }
+  onSubmit() {
+    this.calcGrades()
+  }
+  async calcGrades() {
+    this.answer.grade = "0";
+    this.answer.total = "0";
+    await this.preguntas.forEach((pregunta, index) => {
+      if (pregunta.answer == this.answer.answers[index]) {
+        this.answer.grade = (parseInt(this.answer.grade) + (parseInt(pregunta.value) / 5)) + "";
+      }
+      this.answer.total = (parseInt(this.answer.total) + (parseInt(pregunta.value) / 5)) + "";
+    })
+    this.testService.addAnswer(this.answer).then(ans=>{
+      window.alert("Enviado Perfecto")
+    });
   }
 
 }
@@ -22,4 +41,10 @@ class Pregunta {
   panswer: string[] = ["", "", "", ""];
   answer: string = "";
   value: string = "";
+}
+class Answer {
+  answers: string[] = ["0", "0", "0", "0", "0"];
+  grade: string = "0";
+  total: string = "0";
+  date: Date;
 }
